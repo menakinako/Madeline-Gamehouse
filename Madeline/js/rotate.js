115e9e0e86a5986b0c38df7313d14294eb0b5a2e
Payoja Muduli <72397937+menakinako@users.noreply.github.com>
2021-04-03 23:20:55 +0530

    Delete Madeline directory

--- Madeline/js/rotate.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll(".cards__single");
-
-function flipCard() {
-  // toggle the cards on click
-  this.classList.toggle("flip");
-}
-
-cards.forEach((card) => card.addEventListener("click", flipCard));
